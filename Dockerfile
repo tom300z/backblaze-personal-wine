26ec12a865ba241c004e46353fc422713bd7e7e7
Tom <tom@hoerberg.de>
2020-09-15 17:44:23 +0200

    Added Dockerfile and start script

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+FROM i386/alpine
+
+# Install required packages
+RUN apk --update --no-cache add xvfb x11vnc openbox samba-winbind-clients 
+RUN echo "https://dl-4.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories && \
+    apk --no-cache add wine
+
+# Configure the virtual display port
+ENV DISPLAY :0
+
+# Expose the vnc port
+EXPOSE 5900
+
+# Configure the wine prefix location
+RUN mkdir /wine
+ENV WINEPREFIX /wine/
+
+# Disable wine debug messages
+ENV WINEDEBUG -all
+
+# Configure wine to run without mono or gecko as they are not required
+ENV WINEDLLOVERRIDES mscoree,mshtml=
+
+# Set the wine computer name
+ENV COMPUTER_NAME bz-docker
+
+# Create the data Directory
+RUN mkdir /data
+
+# Copy the start script to the container
+COPY start.sh /start.sh
+
+# Set the start script as entrypoint
+ENTRYPOINT ./start.sh
---
+++ start.sh
@@ -0,0 +1,37 @@
+echo "Starting the virtual display & vnc server"
+rm -f /tmp/.X0-lock
+Xvfb :0 -screen 0 1024x768x24 & openbox & x11vnc -nopw -q -forever -loop -shared &
+
+function configure_wine {
+	unlink $WINEPREFIX/dosdevices/z:
+	ln -s /data/ $WINEPREFIX/dosdevices/d:
+
+
+	if [ ${#COMPUTER_NAME} -gt 15 ]; then 
+		echo "Error: computer name cannot be longer than 15 characters"
+		exit 1
+	fi
+	echo "Setting the wine computer name"
+	wine reg add "HKCU\\SOFTWARE\\Wine\\Network\\" /v UseDnsComputerName /f /d N
+	wine reg add "HKLM\\SYSTEM\\CurrentControlSet\\Control\\ComputerName\\ComputerName" /v ComputerName /f /d $COMPUTER_NAME
+	wine reg add "HKLM\\SYSTEM\\CurrentControlSet\\Control\\ComputerName\\ActiveComputerName" /v ComputerName /f /d $COMPUTER_NAME
+}
+
+until [ -f $WINEPREFIX/drive_c/Program\ Files/Backblaze/bzbui.exe ]; do
+	echo "Backblaze not installed"
+	echo "Initializing the wine prefix"
+	wine wineboot -i -u
+	configure_wine
+	echo "Downloading the Backblaze personal installer..."
+       	wget https://www.backblaze.com/win32/install_backblaze.exe -P /wine/drive_c/
+	echo "Backblaze installer started, please go through the graphical setup in by logging onto the containers vnc server"
+       	wine $WINEPREFIX/drive_c/install_backblaze.exe
+	echo "Installation finished or aborted, trying to start the Backblaze client..."
+	wineserver -k
+done
+
+if [ -f $WINEPREFIX/drive_c/Program\ Files/Backblaze/bzbui.exe ]; then
+	configure_wine
+	echo "Backblaze found, starting the Backblaze client..."
+	wine $WINEPREFIX/drive_c/Program\ Files/Backblaze/bzbui.exe -noqiet
+fi
